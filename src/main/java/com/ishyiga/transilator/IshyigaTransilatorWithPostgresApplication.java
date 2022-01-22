3fee25ef1fb65e0a4a5a9fbee800b2ff60ccf28a
ndatumuremyi <paternenught@gmail.com>
2022-01-22 16:02:12 +0200

    Update IshyigaTransilatorWithPostgresApplication.java

--- src/main/java/com/ishyiga/transilator/IshyigaTransilatorWithPostgresApplication.java
+++ src/main/java/com/ishyiga/transilator/IshyigaTransilatorWithPostgresApplication.java
@@ -3,10 +3,13 @@ package com.ishyiga.transilator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @SpringBootApplication
+@EnableJpaAuditing
 public class IshyigaTransilatorWithPostgresApplication {
 
 	public static void main(String[] args) {
