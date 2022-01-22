80303a9267b736f39f304fe9257f718249de4003
ndatumuremyi <paternenught@gmail.com>
2022-01-22 17:02:41 +0200

    Update TranslationRepository.java

--- src/main/java/com/ishyiga/transilator/repository/TranslationRepository.java
+++ src/main/java/com/ishyiga/transilator/repository/TranslationRepository.java
@@ -2,10 +2,11 @@ package com.ishyiga.transilator.repository;
 
 import com.ishyiga.transilator.model.Translation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 
 @RepositoryRestResource(collectionResourceRel="translation", path="translator")
-public interface TranslationRepository extends JpaRepository<Translation, Integer> {
+public interface TranslationRepository extends CrudRepository<Translation, Integer> {
 }
