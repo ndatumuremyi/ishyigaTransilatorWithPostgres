d5bb3810831afd950d90637c21c86f6187186ece
ndatumuremyi <paternenught@gmail.com>
2022-01-22 17:08:39 +0200

    Update TranslationRepository.java

--- src/main/java/com/ishyiga/transilator/repository/TranslationRepository.java
+++ src/main/java/com/ishyiga/transilator/repository/TranslationRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @RepositoryRestResource(collectionResourceRel="translation", path="translator")
+@CrossOrigin(origins = "*")
 public interface TranslationRepository extends CrudRepository<Translation, Integer> {
 }
